e406a335a32000efe625e091618e47d85a34a5d7
faidnawa <faidnawa@gmail.com>
2017-05-08 07:21:27 +0700

    untuk verisikasi user

---
+++ apii/verify.php
@@ -0,0 +1,27 @@
+<?php
+	include "koneksi.php";
+
+	if(empty($_GET['code']))
+	{
+header("Location: /");
+	}
+$statusY = "Y";
+$statusN = "N";
+$a= $_GET["code"];
+	$tampil= ("SELECT userStatus, token FROM users where token = '".$a."'");
+	$sql = mysql_query($tampil);
+	while($data = mysql_fetch_array($sql))
+	
+		if($data['userStatus']==$statusN)
+		{
+		$query = mysql_query("UPDATE users SET userStatus= '".$statusY."' where token = '".$a."'");
+		echo "selamat anda sudah ter aktifasi silahkan login";
+
+		}
+		else
+		{
+			echo "akun anda sudah aktif silahkan login";
+		}
+		
+
+
