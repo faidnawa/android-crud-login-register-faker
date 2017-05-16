55dc4911e4ac09d1adc209ccc39b8cef9e419a80
fallennymous <fallennymous@gmail.com>
2017-05-16 23:49:53 +0700

    update server

--- app/src/main/java/tk/faidnawa/latihanandroidcrud/app/AppConfig.java
+++ app/src/main/java/tk/faidnawa/latihanandroidcrud/app/AppConfig.java
@@ -2,16 +2,16 @@ package tk.faidnawa.latihanandroidcrud.app;
 
 public class AppConfig {
 	// Server user login url
-	public static String URL_LOGIN = "https://latihanandroid2.000webhostapp.com/login.php";
+	public static String URL_LOGIN = "http://admin-appcrud.000webhostapp.com/login.php";
 	//public static String URL_LOGIN = "http://192.168.56.1/apiii/apii/login.php";
 	// Server user register CEKEMAIL
 	//public static String URL_CEKEMAIL = "https://latihanandroid2.000webhostapp.com/lupapass.php";
 	//public static String URL_CEKEMAIL = "http://192.168.56.1/apiii/apii/lupapass.php";
-	public static String URL_CEKEMAIL = "https://latihanandroid2.000webhostapp.com/lupapass.php";
+	public static String URL_CEKEMAIL = "http://admin-appcrud.000webhostapp.com/lupapass.php";
 	//public static String URL_RESETPASS = "http://192.168.56.1/apiii/apii/resetpass.php";
-	public static String URL_RESETPASS = "https://latihanandroid2.000webhostapp.com/resetpass.php";
+	public static String URL_RESETPASS = "http://admin-appcrud.000webhostapp.com/resetpass.php";
 	// Server user register url
-	public static String URL_REGISTER = "https://latihanandroid2.000webhostapp.com/reg.php";
+	public static String URL_REGISTER = "http://admin-appcrud.000webhostapp.com/reg.php";
 	//public static String URL_REGISTER = "http://192.168.56.1/apiii/apii/reg.php";
 
 
