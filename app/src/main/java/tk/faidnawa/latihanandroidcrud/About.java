7ac8b7a12d12f1b0aa0bbf7152087302442d8106
faidnawa <faidnawa@gmail.com>
2017-05-20 21:40:08 +0700

    modifikasi about

--- app/src/main/java/tk/faidnawa/latihanandroidcrud/About.java
+++ app/src/main/java/tk/faidnawa/latihanandroidcrud/About.java
@@ -1,7 +1,11 @@
 package tk.faidnawa.latihanandroidcrud;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class About extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+
 }
