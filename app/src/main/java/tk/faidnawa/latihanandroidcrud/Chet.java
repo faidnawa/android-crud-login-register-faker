a422359609af9b12c80cebed5091a24162f2dcb8
faidnawa <faidnawa@gmail.com>
2017-05-20 21:40:30 +0700

    modifikasi chat

--- app/src/main/java/tk/faidnawa/latihanandroidcrud/Chet.java
+++ app/src/main/java/tk/faidnawa/latihanandroidcrud/Chet.java
@@ -1,13 +1,22 @@
 package tk.faidnawa.latihanandroidcrud;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class Chet extends AppCompatActivity {
+public class chet extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chet);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }
