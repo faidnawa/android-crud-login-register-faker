658cd355013ce1e8136ecf38187061adb485db1d
faidnawa <faidnawa@gmail.com>
2017-05-22 21:55:40 +0700

    tambah aktivity userdetail utuk chet

---
+++ app/src/main/java/tk/faidnawa/latihanandroidcrud/userDetail.java
@@ -0,0 +1,11 @@
+package tk.faidnawa.latihanandroidcrud;
+
+/**
+ * Created by faidl on 5/21/2017.
+ */
+
+public class userDetail {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
