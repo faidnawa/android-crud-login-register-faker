33e0b4cd5b6274bccb6da37c36595333b6051277
faidnawa <faidnawa@gmail.com>
2017-05-22 21:56:33 +0700

    tambah aktivity chat

---
+++ app/src/main/java/tk/faidnawa/latihanandroidcrud/userchat.java
@@ -0,0 +1,109 @@
+package tk.faidnawa.latihanandroidcrud;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class userchat extends AppCompatActivity {
+    ListView usersList;
+    TextView noUsersText;
+    ArrayList<String> al = new ArrayList<>();
+    int totalUsers = 0;
+    ProgressDialog pd;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userchat);
+        usersList = (ListView)findViewById(R.id.usersList);
+        noUsersText = (TextView)findViewById(R.id.noUsersText);
+        pd = new ProgressDialog(userchat.this);
+        pd.setMessage("Loading...");
+        pd.show();
+
+        String url = "https://androidchatapp-df759.firebaseio.com/users.json";
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+            @Override
+            public void onResponse(String s) {
+                doOnSuccess(s);
+            }
+        },new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                System.out.println("" + volleyError);
+            }
+        });
+
+        RequestQueue rQueue = Volley.newRequestQueue(userchat.this);
+        rQueue.add(request);
+
+        usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                userDetail.chatWith = al.get(position);
+                startActivity(new Intent(userchat.this, chet.class));
+            }
+        });
+    }
+
+    public void doOnSuccess(String s){
+        try {
+            JSONObject obj = new JSONObject(s);
+
+            Iterator i = obj.keys();
+            String key = "";
+
+            while(i.hasNext()){
+                key = i.next().toString();
+
+                if(!key.equals(userDetail.username)) {
+                    al.add(key);
+                }
+
+                totalUsers++;
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if(totalUsers <=1){
+            noUsersText.setVisibility(View.VISIBLE);
+            usersList.setVisibility(View.GONE);
+        }
+        else{
+            noUsersText.setVisibility(View.GONE);
+            usersList.setVisibility(View.VISIBLE);
+            usersList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al));
+        }
+
+        pd.dismiss();
+
+
+    }
+}
